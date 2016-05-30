3d2b97dfe9521c5fee35f236f10e3465390ced43
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2016-05-30 07:57:34 +0200

    Now working on 0.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
-version = (0, 0, 0)
+version = (0, 0, 1)
 str_version = '.'.join(str(v) for v in version)
 
 
